1c5db9780b5f294599cc93c875e6dd829e4a350c
nkp1111 <neerajp4321@gmail.com>
2023-03-22 17:12:44 +0530

    feat: add user schema via sanity

---
+++ app_backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: "user",
+  type: "document",
+  title: "User",
+  fields: [
+    {
+      name: "username",
+      type: "string",
+      title: "Username",
+    },
+    {
+      name: "image",
+      type: "string",
+      title: "Image",
+    }
+  ]
+}
