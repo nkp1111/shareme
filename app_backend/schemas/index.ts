1cdf2d339c14851d2d2b9878a1416b6086c78da1
nkp1111 <neerajp4321@gmail.com>
2023-03-22 18:00:42 +0530

    feat: add pin, save, comment schema

--- app_backend/schemas/index.ts
+++ app_backend/schemas/index.ts
@@ -1,3 +1,7 @@
 import user from './user'
+import pin from './pin'
+import postedBy from './postedBy'
+import comment from './comment'
+import save from './save'
 
-export const schemaTypes = [user]
+export const schemaTypes = [user, pin, postedBy, comment, save]
---
+++ app_backend/schemas/pin.js
@@ -0,0 +1,57 @@
+export default {
+  name: "pin",
+  type: "document",
+  title: "Pin",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title"
+    },
+    {
+      name: "about",
+      type: "string",
+      title: "About"
+    },
+    {
+      name: "destination",
+      type: "url",
+      title: "Destination"
+    },
+    {
+      name: "category",
+      type: "string",
+      title: "Category"
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image",
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: "userId",
+      type: "string",
+      title: "UserID"
+    },
+    {
+      name: "postedBy",
+      type: "postedBy",
+      title: "PostedBy",
+    },
+    {
+      name: "save",
+      type: "array",
+      title: "Save",
+      of: [{ type: "save" }]
+    },
+    {
+      name: "comment",
+      type: "array",
+      title: "Comment",
+      of: [{ type: "comment" }]
+    },
+  ]
+}
