ccebf9652d0bb59578b3f07b8e911d95593208bf
nkp1111 <neerajp4321@gmail.com>
2023-03-23 16:03:25 +0530

    feat: get user info from login response

--- app_frontend/src/components/Login.js
+++ app_frontend/src/components/Login.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import { GoogleLogin } from '@react-oauth/google'
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google'
 import { useNavigate } from 'react-router-dom'
+import jwt_decode from "jwt-decode";
+
 import { FcGoogle } from 'react-icons/fc'
 import logoWhite from "../assets/logowhite.png"
 import shareVideo from '../assets/share.mp4'
 
-import jwt_decode from "jwt-decode";
 
 const Login = () => {
 
   const addUser = (userinfo) => {
-    console.log(userinfo);
+    console.log(userinfo)
+    localStorage.setItem("user", JSON.stringify(userinfo))
+
+    const { name, picture, sub: googleId } = userinfo
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      username: name,
+      image: picture
+    }
   }
 
   return (
@@ -27,9 +37,11 @@ const Login = () => {
       </div>
       {/* login button  */}
       <div className="absolute flex flex-col justify-center items-center top-0 bottom-0 right-0 left-0 bg-blackOverlay">
+        {/* logo  */}
         <div className="p-5">
           <img src={logoWhite} alt="logo" width="130px" />
         </div>
+        {/* google login button  */}
         <div className="shadow-2xl">
           <GoogleLogin
             render={(renderProps) => (
@@ -39,7 +51,7 @@ const Login = () => {
                 onClick={renderProps.onClick}
                 disabled={renderProps.disabled}
               >
-                <FcGoogle className='mr-4' /> Sign in With google
+                <FcGoogle className='mr-4' /> Sign in with Google
               </button>
             )}
             onSuccess={response => {
