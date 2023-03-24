d4bbe22e86fa18fa515fef3aa9d47fed2a72acb7
nkp1111 <neerajp4321@gmail.com>
2023-03-24 17:41:54 +0530

    feat: get feed data for pin viewing

--- app_frontend/src/components/Feed.js
+++ app_frontend/src/components/Feed.js
@@ -4,15 +4,45 @@ import { useParams } from 'react-router-dom'
 import { client } from '../client'
 import MasonryLayout from './MasonryLayout'
 import Spinner from './Spinner'
+import { searchQuery, feedQuery } from '../utils/data'
 
 const Feed = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState([]);
+  const { categoryId } = useParams()
+
+  useEffect(() => {
+    setLoading(true)
+    if (categoryId) {
+      const query = searchQuery(categoryId)
+      client.fetch(query)
+        .then(data => {
+          setPins(data)
+          setLoading(false)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    } else {
+      const query = feedQuery()
+      client.fetch(query)
+        .then(data => {
+          setPins(data)
+          setLoading(false)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }, [categoryId]);
+
   if (loading) {
     return <Spinner message="We are adding new ideas to your feed!" />
   }
+
   return (
     <div>
-      Feed
+      {pins && <MasonryLayout pins={pins} />}
     </div>
   )
 }
