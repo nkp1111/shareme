51f4d3a7d45d6b7bcd32b8c63a913a5162844a07
nkp1111 <neerajp4321@gmail.com>
2023-03-24 10:07:14 +0530

    feat: add sidebar toggle

--- app_frontend/src/container/Home.js
+++ app_frontend/src/container/Home.js
@@ -33,12 +33,12 @@ const Home = () => {
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height ease-out duration-75'>
 
       <div className="hidden md:flex h-screen flex-initial">
-        <Sidebar />
+        <Sidebar user={user && user} />
       </div>
 
       <div className="flex md:hidden flex-row">
         <HiMenu fontSize={40} className="cursor-pointer"
-          onClick={() => setToggleSidebar(!toggleSidebar)} />
+          onClick={() => setToggleSidebar(true)} />
         <Link to={"/"}>
           <img src={logo} alt="logo" className='w-28' />
         </Link>
@@ -46,7 +46,16 @@ const Home = () => {
           <img src={user?.image} alt="avatar" className='w-28' />
         </Link>
       </div>
-      <UserProfile />
+
+      {toggleSidebar && (
+        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+          <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle fontSize={30} className="cursor-pointer"
+              onClick={() => setToggleSidebar(false)} />
+          </div>
+          <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+        </div>
+      )}
     </div>
   )
 }
